c610ca2a76458cf98d753198c7e536f0a74bc9a1
Equinox4 <github@jfouquet.fr>
2020-05-26 16:22:58 +0200

    renamed to match with interfaces names

--- src/app/ports/CEPBusManagementInboundPort.java
+++ src/app/ports/CEPBusManagementInboundPort.java
@@ -1,40 +1,35 @@
 package app.ports;
 
+import fr.sorbonne_u.components.ComponentI;
+import fr.sorbonne_u.components.ports.AbstractInboundPort;
+
 import app.components.CEPBus;
 import app.interfaces.bus.CEPBusManagementCI;
 import app.interfaces.bus.EventEmissionCI;
-import app.interfaces.bus.EventReceptionCI;
 import app.interfaces.events.EventI;
-import fr.sorbonne_u.components.ComponentI;
-import fr.sorbonne_u.components.ports.AbstractInboundPort;
-import fr.sorbonne_u.components.ports.AbstractOutboundPort;
 
-
-public class CEPBusOutboundPort extends AbstractOutboundPort implements CEPBusManagementCI{
-	
-	/**
-	 * 
-	 */
+public class CEPBusManagementInboundPort extends AbstractInboundPort implements CEPBusManagementCI {
 	private static final long serialVersionUID = 1L;
 
-	public CEPBusOutboundPort(ComponentI owner) throws Exception {
+	public CEPBusManagementInboundPort(ComponentI owner) throws Exception {
 		super(CEPBusManagementCI.class, owner);
+		assert owner instanceof CEPBus;
 	}
 
-	public CEPBusOutboundPort(String uri, ComponentI owner) throws Exception {
+	public CEPBusManagementInboundPort(String uri, ComponentI owner) throws Exception {
 		super(uri, CEPBusManagementCI.class, owner);
+		assert owner instanceof CEPBus;
 	}
 
-	protected CEPBusOutboundPort(Class<?> implementedInterface, ComponentI owner)
-			throws Exception {
+	protected CEPBusManagementInboundPort(Class<?> implementedInterface, ComponentI owner) throws Exception {
 		super(implementedInterface, owner);
+		assert owner instanceof CEPBus;
 	}
 
-	protected CEPBusOutboundPort(String uri, Class<?> implementedInterface, ComponentI owner)
-			throws Exception {
+	protected CEPBusManagementInboundPort(String uri, Class<?> implementedInterface, ComponentI owner) throws Exception {
 		super(uri, implementedInterface, owner);
+		assert owner instanceof CEPBus;
 	}
-	
 
 	@Override
 	public String getEventReceptionInboundPortURI(String uri) throws Exception {
@@ -45,19 +40,16 @@ public class CEPBusOutboundPort extends AbstractOutboundPort implements CEPBusMa
 	@Override
 	public void registerEventReceptor(String uri, String inboundPortURI) throws Exception {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void unregisterEventReceptor(String uri) throws Exception {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void registerCommandExecutor(String uri, String inboundPortURI) throws Exception {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -69,8 +61,5 @@ public class CEPBusOutboundPort extends AbstractOutboundPort implements CEPBusMa
 	@Override
 	public void unregisterCommandExecutor(String uri) throws Exception {
 		// TODO Auto-generated method stub
-		
 	}
-	
-
 }
--- src/app/ports/CEPBusManagementOutboundPort.java
+++ src/app/ports/CEPBusManagementOutboundPort.java
@@ -1,42 +1,33 @@
 package app.ports;
 
+import fr.sorbonne_u.components.ComponentI;
+import fr.sorbonne_u.components.ports.AbstractInboundPort;
+import fr.sorbonne_u.components.ports.AbstractOutboundPort;
+
 import app.components.CEPBus;
 import app.interfaces.bus.CEPBusManagementCI;
 import app.interfaces.bus.EventEmissionCI;
+import app.interfaces.bus.EventReceptionCI;
 import app.interfaces.events.EventI;
-import fr.sorbonne_u.components.ComponentI;
-import fr.sorbonne_u.components.ports.AbstractInboundPort;
 
-
-public class CEPBusInboundPort extends AbstractInboundPort implements CEPBusManagementCI{
-	
-	/**
-	 * 
-	 */
+public class CEPBusManagementOutboundPort extends AbstractOutboundPort implements CEPBusManagementCI {
 	private static final long serialVersionUID = 1L;
 
-	public CEPBusInboundPort(ComponentI owner) throws Exception {
+	public CEPBusManagementOutboundPort(ComponentI owner) throws Exception {
 		super(CEPBusManagementCI.class, owner);
-		assert owner instanceof CEPBus;
 	}
 
-	public CEPBusInboundPort(String uri, ComponentI owner) throws Exception {
+	public CEPBusManagementOutboundPort(String uri, ComponentI owner) throws Exception {
 		super(uri, CEPBusManagementCI.class, owner);
-		assert owner instanceof CEPBus;
 	}
 
-	protected CEPBusInboundPort(Class<?> implementedInterface, ComponentI owner)
-			throws Exception {
+	protected CEPBusManagementOutboundPort(Class<?> implementedInterface, ComponentI owner)	throws Exception {
 		super(implementedInterface, owner);
-		assert owner instanceof CEPBus;
 	}
 
-	protected CEPBusInboundPort(String uri, Class<?> implementedInterface, ComponentI owner)
-			throws Exception {
+	protected CEPBusManagementOutboundPort(String uri, Class<?> implementedInterface, ComponentI owner)	throws Exception {
 		super(uri, implementedInterface, owner);
-		assert owner instanceof CEPBus;
 	}
-
 
 	@Override
 	public String getEventReceptionInboundPortURI(String uri) throws Exception {
@@ -47,19 +38,16 @@ public class CEPBusInboundPort extends AbstractInboundPort implements CEPBusMana
 	@Override
 	public void registerEventReceptor(String uri, String inboundPortURI) throws Exception {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void unregisterEventReceptor(String uri) throws Exception {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void registerCommandExecutor(String uri, String inboundPortURI) throws Exception {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -71,6 +59,5 @@ public class CEPBusInboundPort extends AbstractInboundPort implements CEPBusMana
 	@Override
 	public void unregisterCommandExecutor(String uri) throws Exception {
 		// TODO Auto-generated method stub
-		
 	}
 }
