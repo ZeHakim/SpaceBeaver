410eb94630eb8262c8bdb0d5659cb89434cd629e
Equinox4 <github@jfouquet.fr>
2020-05-27 00:08:46 +0200

    renommage

--- src/app/components/CEPBus.java
+++ src/app/components/CEPBus.java
@@ -1,5 +1,9 @@
 package app.components;
 
+import fr.sorbonne_u.components.AbstractComponent;
+import fr.sorbonne_u.components.annotations.OfferedInterfaces;
+import fr.sorbonne_u.components.annotations.RequiredInterfaces;
+
 import app.interfaces.bus.CEPBusManagementCI;
 import app.interfaces.bus.EventEmissionCI;
 import app.interfaces.bus.EventReceptionCI;
@@ -9,24 +13,21 @@ import app.ports.CEPBusOutboundPort;
 import app.ports.EventReceptionInboundPort;
 import app.ports.PresenceDetectorInboundPort;
 import app.ports.PresenceDetectorOutboundPort;
-import fr.sorbonne_u.components.AbstractComponent;
-import fr.sorbonne_u.components.annotations.OfferedInterfaces;
-import fr.sorbonne_u.components.annotations.RequiredInterfaces;
 
 
 @OfferedInterfaces(offered = {CEPBusManagementCI.class, EventReceptionCI.class})
-@RequiredInterfaces(required = {CEPBusManagementCI.class,EventEmissionCI.class})
+@RequiredInterfaces(required = {EventEmissionCI.class})
 public class CEPBus extends AbstractComponent {
-	
+
 	public static final String INBOUND_PORT_URI = "busiURI";
 	protected CEPBusInboundPort busi;
-	protected EventReceptionInboundPort erbp;
+	protected EventReceptionInboundPort erip;
 
 	protected CEPBus() throws Exception {
 		super(1, 0);
 		this.initialise();
 	}
-	
+
 	protected CEPBus(String reflectionInboundPortURI) throws Exception {
 		super(reflectionInboundPortURI, 1, 0);
 		this.initialise();
@@ -35,14 +36,14 @@ public class CEPBus extends AbstractComponent {
 	protected void initialise() throws Exception {
 		this.busi = this.createPort();
 		//this.busi.publishPort();
-		this.erbp = new EventReceptionInboundPort(this);
-		//this.erbp.publishPort();
+		this.erip = new EventReceptionInboundPort(this);
+		//this.erip.publishPort();
 	}
-	
+
 	@Override
 	public void execute() throws Exception {
 		System.out.println("gggggg");
-		System.out.println("eeeeeeee "+erbp.getPortURI());
+		System.out.println("eeeeeeee "+erip.getPortURI());
 	}
 
 	protected CEPBusInboundPort createPort() throws Exception {
@@ -52,7 +53,4 @@ public class CEPBus extends AbstractComponent {
 	public void sendEvent(String emitterURI, String destinationURI, EventI e) {
 		System.out.println("Event recu !");
 	}
-	
-	
-
 }
